1e6d5dd2eff7613008ceb39d6984dbe182250b82
dev369-ss <sadhna369.dev@gmail.com>
2026-09-22 15:03:35 +0000

    add user model

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -17,12 +17,31 @@ const userSchema = new mongoose.Schema({
         type:String,
         trim:true
     },
-    {
-        
+    password:{
+        required:true,
+        type:String
+    },
+    role:{
+        required:true,
+        type:String,
+        enum:["user", "admin"],
+        default: "user"
     }
 }, {
     timestamps: true
-})
+});
+
+userSchema.pre("save", async (next) =>{
+  const salt = await bcrypt.genSalt(10);
+  this.password= await bcrypt.hash(this.password,salt)
+  next()
+});
+
+userSchema.methods.comparePassword= async (dataPassword) =>{
+  return await bcrypt.compare(dataPassword, this.password);
+}
+
+module.exports = mongoose.model("User", userSchema);
 
 
 
